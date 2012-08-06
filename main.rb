bc38ec2493363fba223a568d861889f9a359d163
okor <jason.ormand1@gmail.com>
2012-08-06 08:53:37 -0400

    more best practices and refactoring, may have broke something

--- main.rb
+++ main.rb
@@ -25,7 +25,7 @@ helpers do
 			:domain => lookup_info.domain,
 			:created_on => lookup_info.created_on,
 			:expires_on => lookup_info.expires_on,
-			:whois_server => lookup_info.referral_whois,
+			:whois_servers => lookup_info.referral_whois,
 			:nameservers => lookup_info.nameservers,
 			:admin_contacts => admin_contacts,
 			:techical_contacts => technical_contacts
@@ -36,20 +36,15 @@ end
 
 
 get '/' do
-	cache_for_day
+	# cache_for_day
 	haml :index
-end
-
-
-get '/ip.json' do
-	{ :ip => request.ip }.to_json
 end
 
 
 get '/lookup' do
 	begin
-		cache_for_day
-		@formatted_response = whois_lookup
+		# cache_for_day
+		@whois = whois_lookup
 		haml :lookup
 	rescue
 		haml :error
@@ -59,7 +54,7 @@ end
 
 get '/lookup.json' do
 	begin
-		cache_for_day
+		# cache_for_day
 		whois_lookup.to_json
 	rescue
 		{:Error => 'Bad Request'}.to_json
